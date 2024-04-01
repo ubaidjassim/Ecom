4a3df0077dbf40c0b62ea75531bd9632a62a8140
ubaidjassim <ubaidjassim1842@gmail.com>
2024-04-01 23:33:19 +0530

    UntilNav

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let toggleBTN = document.querySelector(".toggleMenu")
+let sideNav = document.querySelector(".side-nav")
+let crossBTN = document.querySelector(".cross")
+
+
+toggleBTN.addEventListener("click", () => {
+    sideNav.style.left = "0"
+})
+crossBTN.addEventListener("click", () => {
+    sideNav.style.left = "-60%"
+})
 
