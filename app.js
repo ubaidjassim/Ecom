c820f3c7baa173094fd34a3ace56af92bc7b46f4
ubaidjassim <ubaidjassim1842@gmail.com>
2024-04-02 19:20:43 +0530

    rough

--- app.js
+++ app.js
@@ -10,3 +10,21 @@ crossBTN.addEventListener("click", () => {
     sideNav.style.left = "-60%"
 })
 
+let search = document.querySelector("#search")
+let productContainer = document.querySelector(".products")
+let productList = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", (event) => {
+    let enteredValue = event.target.value.toUpperCase()
+
+    for (let count = 0; count < productList.length; count++) {
+
+        let productName = productList[count].querySelector("p").textContent.toUpperCase()
+
+        if (productName.indexOf(enteredValue) < 0) {
+            productList[count].style.display = "none"
+        } else {
+            productList[count].style.display = "block"
+        }
+    }
+})
